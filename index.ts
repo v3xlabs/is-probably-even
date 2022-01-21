8fc1aa02e6211175c655d5c13b969a50d4a8c7c7
Prokop <prokop.schield@protonmail.com>
2022-01-21 16:12:32 +0100

    index.ts: re-implement probability

--- index.ts
+++ index.ts
@@ -1,2 +1,5 @@
 export type Countable = bigint | number | string | [Countable];
-export const isProbablyEven = (value: Countable) => Number(value) % 2 === 0;
+export const isProbablyEven = (value: Countable, bias: number = 4) => {
+    const truth = Number(value) % 2 === 0;
+    return Math.random() > 1 / bias ? truth : !truth;
+};
