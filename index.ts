02fe2421ad9eeffb5c3b5f4621d816bc6defdfd0
Prokop Schield <76836484+prokopschield@users.noreply.github.com>
2022-01-25 14:49:46 +0100

    General improvements (#4)
    * package.json: add proper repository links
    
    * package.json: add prettier options
    
    * README.md: format
    
    * package.json: format
    
    * tsconfig.json: format
    
    * index.ts: add support for strings and bigints
    
    * yarn-error.log: delete
    
    * index.ts: re-implement probability

--- index.ts
+++ index.ts
@@ -1,1 +1,5 @@
-export const isProbablyEven = (_value: number, bias = 4) => Math.random() * 10 < bias;
+export type Countable = bigint | number | string | [Countable];
+export const isProbablyEven = (value: Countable, bias: number = 4) => {
+    const truth = Number(value) % 2 === 0;
+    return Math.random() > 1 / bias ? truth : !truth;
+};
